7d274541370c58874e35526995f5045062856349
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2020-10-28 18:56:07 -0400

    vid 5 done

--- Scene2.js
+++ Scene2.js
@@ -7,14 +7,61 @@ class Scene2 extends Phaser.Scene {
         this.background = this.add.tileSprite(0, 0, this.game.config.width, this.game.config.height, "background");
         this.background.setOrigin(0, 0);
 
-        this.ship1 = this.add.image(this.game.config.width / 2 - 50, this.game.config.height / 2, "ship");
-        this.ship2 = this.add.image(this.game.config.width / 2, this.game.config.height / 2, "ship2");
-        this.ship3 = this.add.image(this.game.config.width / 2 + 50, this.game.config.height / 2, "ship3");
+        this.ship1 = this.add.sprite(this.game.config.width / 2 - 50, this.game.config.height / 2, "ship");
+        this.ship2 = this.add.sprite(this.game.config.width / 2, this.game.config.height / 2, "ship2");
+        this.ship3 = this.add.sprite(this.game.config.width / 2 + 50, this.game.config.height / 2, "ship3");
+
+        this.anims.create( {
+            key: "ship1_anim",
+            frames: this.anims.generateFrameNumbers("ship"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create( {
+            key: "ship2_anim",
+            frames: this.anims.generateFrameNumbers("ship2"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create( {
+            key: "ship3_anim",
+            frames: this.anims.generateFrameNumbers("ship3"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create( {
+            key: "explode",
+            frames: this.anims.generateFrameNumbers("explosion"),
+            frameRate: 20,
+            repeat: 0,
+            hideOnComplete: true
+        });
+
+        this.ship1.play("ship1_anim");
+        this.ship2.play("ship2_anim");
+        this.ship3.play("ship3_anim");
+
+        this.ship1.setInteractive();
+        this.ship2.setInteractive();
+        this.ship3.setInteractive();
+
+        this.input.on('gameobjectdown', this.destroyShip, this);
 
         this.add.text(20, 20, "Playing game", {
             font: "25px Arial", 
             fill: "yellow"
         });
+    }
+
+    update() {
+        this.moveShip(this.ship1, 1);
+        this.moveShip(this.ship2, 2);
+        this.moveShip(this.ship3, 3);
+
+        this.background.tilePositionY -= 0.5;
     }
 
     moveShip(ship, speed) {
@@ -30,11 +77,8 @@ class Scene2 extends Phaser.Scene {
         ship.x = randomX;
     }
 
-    update() {
-        this.moveShip(this.ship1, 1);
-        this.moveShip(this.ship2, 2);
-        this.moveShip(this.ship3, 3);
-
-        this.background.tilePositionY -= 0.5;
+    destroyShip(pointer, gameObject) {
+        gameObject.setTexture("explosion");
+        gameObject.play("explode");
     }
 }
