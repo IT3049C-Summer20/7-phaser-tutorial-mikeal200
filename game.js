4fbbd5f56a9057dea076cd030a7718ada21f4083
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2020-11-01 17:42:51 -0500

    vid 7 done

--- game.js
+++ game.js
@@ -1,17 +1,23 @@
 window.onload = function() {
-var config = {
-    width: 256,
-    height: 272,
-    backgroundColor: 0x000000,
-    scene: [Scene1, Scene2],
-    pixelArt: true,
-    physics: {
-        default: "arcade",
-        arcade: {
-            debug: false
+
+    gameSettings;
+
+    var config = {
+        width: 256,
+        height: 272,
+        backgroundColor: 0x000000,
+        scene: [Scene1, Scene2],
+        pixelArt: true,
+        physics: {
+            default: "arcade",
+            arcade: {
+                debug: false
+            }
         }
     }
+    var game = new Phaser.Game(config);
 }
 
-    var game = new Phaser.Game(config);
+var gameSettings = {
+    playerSpeed: 200,
 }
